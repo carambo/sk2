80163aed9956023d24d8b3830aa6c28f936999fc
carambo <marpecko@gmail.com>
2014-12-07 15:37:12 +0100

    Class Lorry edited

--- parkovisko_evidencia/src/parking/house/model/Lorry.java
+++ parkovisko_evidencia/src/parking/house/model/Lorry.java
@@ -1,8 +1,15 @@
 package parking.house.model;
 
-public class Lorry {
+
+public class Lorry extends Vehicle {
 	
 	int maxLoad;
+
+	public Lorry(DrivingLicenseType dlt, int numberOfWheels,
+			String licensePlate, String type, int maxLoad) {
+		super(dlt, maxLoad, licensePlate, licensePlate);
+		this.maxLoad = maxLoad;
+	}
 	
 	
 	
