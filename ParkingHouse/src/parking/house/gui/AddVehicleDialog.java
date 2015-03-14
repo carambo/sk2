aee414e37c92dd34da653605617506070a0362d7
carambo <finepi47@azet.sk>
2015-03-14 19:36:39 +0100

    AddVehicleDialog changed, new input window just for test, now I have to update ParkingHouseGUI - program is not working in this point

--- ParkingHouse/src/parking/house/gui/AddVehicleDialog.java
+++ ParkingHouse/src/parking/house/gui/AddVehicleDialog.java
@@ -10,6 +10,7 @@ import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.graphics.Point;
 
 import parking.house.model.Bus;
@@ -51,37 +52,45 @@ public class AddVehicleDialog extends Dialog {
 
 		final Combo combo = new Combo(shell, SWT.READ_ONLY);
 		combo.setItems(new String[] { "Bus", "Lorry", "Car", "Motorcycle" });
+		combo.setLocation(new Point(20, 20));
 		Rectangle clientArea = shell.getClientArea();
 		combo.setBounds(clientArea.x, clientArea.y, 400, 400);
 
+		final Text t = new Text(shell, SWT.BORDER | SWT.MULTI);
+		t.setText("enter license plate");
+		t.setSize(200, 25);
+		t.setLocation(new Point(20, 40));
+		
+
 		Button ok = new Button(shell, SWT.PUSH);
-		ok.setLocation(new Point(20, 20)); // Point from swt.graphics -imported
+		ok.setLocation(new Point(20, 60)); // Point from swt.graphics -imported
 		ok.setText("ok");
 		ok.addSelectionListener(new SelectionListener() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
 				Vehicle v = null;
+				String s; // mam to, staci si to dostat hentak z gettextu a potom dam premennu normalne ako parameter
+				s = t.getText();
 				if (combo.getText().equals("Bus")) {
-					v = new Bus(DrivingLicenseType.D, 4, "BA421IO", "Karosa",
-							70);
-					// labelvehicInTab.setText("Vehicles in tab: " +
-					// ++vehicInTab);
+					v = new Bus(1, DrivingLicenseType.D, 20, "Karosa", "Yellow",
+							s);
 				} else if (combo.getText().equals("Lorry")) {
-					v = new Lorry(DrivingLicenseType.C, 6, "BA700DD", "Scania",
-							5000);
-					// labelvehicInTab.setText("Vehicles in tab:" +
-					// ++vehicInTab);
+					v = new Lorry(2, DrivingLicenseType.C, 40, "Iveco", "White",
+							s);
 				} else if (combo.getText().equals("Car")) {
-					v = new Car(DrivingLicenseType.B, 4, "BO777SS", "skoda", 2);
-					// labelvehicInTab.setText("Vehicles in tab:" +
-					// ++vehicInTab);
+					v = new Car(3, DrivingLicenseType.B, 50, "Mercedes", "Blue",
+							s);
 				} else if (combo.getText().equals("Motorcycle")) {
-					v = new Motorcycle(DrivingLicenseType.A, 2, "MY000AA",
-							"honda", 200);
+					v = new Motorcycle(4, DrivingLicenseType.A, 60, "Honda", "Red",
+							s);
 				} else {
 					System.out.println("else");
 				}
+			
+
 				vehicle = v;
+			
+				
 				shell.close();
 			}
 
@@ -89,6 +98,11 @@ public class AddVehicleDialog extends Dialog {
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 			}
 		});
+		
+		
 		ok.pack();
 	}
-}
+	
+
+		
+	}
